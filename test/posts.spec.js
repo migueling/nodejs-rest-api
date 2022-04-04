d177c9c4423867a67e0a2e7122746f1419e5311f
Miguel Sanchez <ing.miguels@gmail.com>
2022-04-03 22:00:26 -0500

    Fixed test

--- test/posts.spec.js
+++ test/posts.spec.js
@@ -5,7 +5,7 @@ const app = require("../src/index");
 describe("Calling GET /posts", () => {
   test("It should response with a 200 status code", async () => {
     const response = await request(app).get("/posts").send();
-    expect(response.status).toBe(201);
+    expect(response.status).toBe(200);
   });
 
   test("It should response with an array", async () => {
